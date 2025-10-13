cea1b8643cb0c98d853ca5ec2ec4fe0bce4f7d1a
a1 <sesila94@gmail.ru>
2025-10-13 14:04:05 +0300

    1.0.5 - add usecases

---
+++ app/src/main/java/ru/vlyashuk/pointmap/domain/usecase/UpdatePointUseCase.kt
@@ -0,0 +1,11 @@
+package ru.vlyashuk.pointmap.domain.usecase
+
+import ru.vlyashuk.pointmap.data.repository.PointRepository
+import ru.vlyashuk.pointmap.domain.model.Point
+import javax.inject.Inject
+
+class UpdatePointUseCase @Inject constructor (private val repository: PointRepository) {
+    suspend operator fun invoke(point: Point) {
+        repository.update(point.toEntity())
+    }
+}
--- app/src/main/java/ru/vlyashuk/pointmap/ui/screens/MainScreen.kt
+++ app/src/main/java/ru/vlyashuk/pointmap/ui/screens/MainScreen.kt
@@ -1,5 +1,6 @@
 package ru.vlyashuk.pointmap.ui.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
